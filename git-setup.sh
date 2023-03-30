2bc168019634ce3b2f26c6f27fce905698060de2
saltysaff <d_ash12345@hotmail.co.uk>
2023-03-30 01:06:29 +0100

    add support for eslint and prettier

--- git-setup.sh
+++ git-setup.sh
@@ -19,6 +19,31 @@ cp ~/Scripts/git-setup/templates/index.html .
 touch style.css script.js
 echo "Creating directory and files..."
 
+# Initialize npm project
+npm init -y
+
+# Install ESLint and eslint-config-prettier locally
+npm i eslint eslint-config-prettier -D
+
+# Initialize ESLint configuration
+npx eslint --init
+
+# Add eslint-config-prettier to the ESLint configuration
+ESLINTRC_FILES=(".eslintrc.json" ".eslintrc.js" ".eslintrc.yaml" ".eslintrc.yml" ".eslintrc")
+for FILE in "${ESLINTRC_FILES[@]}"; do
+  if [ -f "$FILE" ]; then
+    if [[ "$FILE" == *".json" ]]; then
+      jq '.extends += ["prettier"]' "$FILE" > ".tmp" && mv ".tmp" "$FILE"
+    elif [[ "$FILE" == *".js" ]]; then
+      sed -i "s/extends: \[/extends: \[\"prettier\", /g" "$FILE"
+    elif [[ "$FILE" == *".yaml" || "$FILE" == *".yml" ]]; then
+      sed -i "s/extends:/extends: [\"prettier\", /g" "$FILE"
+      sed -i "/extends: \[\"prettier\", /a ]" "$FILE"
+    fi
+    break
+  fi
+done
+
 # Initiate git repo and commit all files
 git init -b main
 git add . && git commit -m "Initial commit"
@@ -26,6 +51,3 @@ git add . && git commit -m "Initial commit"
 # Create a public GitHub repo
 gh repo create --source=. --public --push
 echo "Project created and successfully pushed to GitHub!"
-
-
-
